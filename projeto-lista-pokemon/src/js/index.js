43d22f653753f779942fdb893dd3dc791282283f
vhfsilva <30941086+VictoorFerreira@users.noreply.github.com>
2024-09-04 21:20:13 -0300

    Atualização

--- projeto-lista-pokemon/src/js/index.js
+++ projeto-lista-pokemon/src/js/index.js
@@ -18,7 +18,7 @@
 
 const botaoAlterarTema = document.getElementById("botao-alterar-tema");
 const body = document.querySelector("body");
-const imagemBotaoTrocaDeTema = document.querySelector("modo-escuro");
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
 
 botaoAlterarTema.addEventListener("click", () => {
     const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
